f2cb03cc2938180aebb960b2ba11188325ee38a9
McLeopold <mcleopold@gmail.com>
2017-09-13 21:31:53 -0700

    Add messages list
    Updated app module to include minimal functionality for saving a messages list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLZo_A2bqgSywFrBcl_Fmm7sphJ7dbNgY",
+  authDomain: "chessrank-5dc6c.firebaseapp.com",
+  databaseURL: "https://chessrank-5dc6c.firebaseio.com",
+  projectId: "chessrank-5dc6c",
+  storageBucket: "chessrank-5dc6c.appspot.com",
+  messagingSenderId: "158770539671"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
