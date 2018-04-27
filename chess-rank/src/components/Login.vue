dafa4dc168cb1cfd0b56e891c8002ba9799e37ad
McLeopold <mcleopold@gmail.com>
2018-04-26 20:42:02 -0700

    Add Login and SignUp routes

---
+++ chess-rank/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="Email"><br />
+        <input type="password" placeholder="Password"><br />
+        <button v-on:click="login">Connection</button>
+        <p>Create account or <router-link to="/sign-up">signup</router-link></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data: function() {
+            return {}
+        },
+        methods: {
+            login: function () {
+                this.$router.replace('helloworld')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ chess-rank/src/components/SignUp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="sign-up">
+        <p>create account</p>
+        <input type="text" placeholder="Email"><br />
+        <input type="password" placeholder="Password"><br />
+        <button>Sign Up</button>
+        <span>or go back to <router-link to="/login">login</router-link></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'signUp',
+        data: function() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+  .signUp {
+    margin-top: 40px;
+  }
+</style>
