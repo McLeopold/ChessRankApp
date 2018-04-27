97c4c81ba69fb4378639275e1f3bc88b6e60a69b
McLeopold <mcleopold@gmail.com>
2018-04-26 21:45:40 -0700

    Add login to firebase
    Connected firebase login to login page

--- chess-rank/src/components/Login.vue
+++ chess-rank/src/components/Login.vue
@@ -1,22 +1,34 @@
 <template>
     <div class="login">
         <h3>Sign In</h3>
-        <input type="text" placeholder="Email"><br />
-        <input type="password" placeholder="Password"><br />
-        <button v-on:click="login">Connection</button>
+        <input type="text" v-model="email" placeholder="Email"><br />
+        <input type="password" v-model="password" placeholder="Password"><br />
+        <button v-on:click="signIn">Connection</button>
         <p>Create account or <router-link to="/sign-up">signup</router-link></p>
     </div>
 </template>
 
 <script>
+    import firebase from 'firebase'
+
     export default {
         name: 'login',
         data: function() {
-            return {}
+            return {
+                email: '',
+                password: ''
+            }
         },
         methods: {
-            login: function () {
-                this.$router.replace('helloworld')
+            signIn: function () {
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+                    function(user) {
+                        console.log('Connected');
+                    },
+                    function (err) {
+                        console.log('Opps: ' + err.message);
+                    }
+                )
             }
         }
     }
